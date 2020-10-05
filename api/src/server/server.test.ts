0e0d5ce58fa1c42df9ff0af076cb1495e5e43ebe
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-10-05 21:20:36 +0100

    Enables unit testing

---
+++ api/src/server/server.test.ts
@@ -0,0 +1,34 @@
+import { expect } from 'chai';
+import { agent as request } from 'supertest';
+
+import { Server } from './server';
+
+/*tslint:disable: no-unused-expression */
+
+describe('Server Test', () => {
+
+    let server: Server;
+
+    before((done) => {
+        server = new Server({ port: '9999' });
+        server.initServer().then(() => {
+            done();
+        });
+    });
+
+    it('should always pass', () => {
+        expect(true).to.equal(true);
+    });
+
+    it('should GET /', async () => {
+        const res = await request(server.app)
+            .get('/');
+
+        expect(res.status).to.equal(200);
+        expect(res.text).not.to.be.empty;
+        expect(res.text).to.be.equal('Hello World');
+
+    });
+
+
+});
